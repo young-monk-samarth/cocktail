63cb0ac1f683db314801fe354ec1e0ffc15b959c
Samarth Muktamath <samarthmise2025@gmail.com>
2025-07-17 20:32:15 +0530

    implent ftrst hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { gsap } from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
-
+import Navbar from "./components/Navbar";
+import  Hero  from "./components/Hero";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div ><h1 className="text-3xl text-indigo-300">Hello World</h1></div>
+    <main>
+      <Navbar />
+      <Hero/>
+    </main>
   );
 }
 
