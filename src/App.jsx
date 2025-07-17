b2e6aaed6a55a77e9b3e45bca9a95d40426d5786
Samarth Muktamath <samarthmise2025@gmail.com>
2025-07-17 22:39:04 +0530

    implent contact

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Navbar from "./components/Navbar";
 import  Hero  from "./components/Hero";
 import Cocktails from "./components/Cocktail";
 import About from "./components/About";
+import Art from "./components/Art";
+import Menu from "./components/Menu";
+import Contact from "./components/Contact";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
@@ -13,6 +16,9 @@ const App = () => {
       <Hero/>
       <Cocktails />
       <About />
+    < Art/>
+    <Menu/>
+    <Contact />
     </main>
   );
 }
